d346e0e4ccbd71d1a60af9cc3111710277dfd6af
ekan07 <aniekanbon@gmail.com>
2023-02-02 21:20:17 +0100

    Add tasks 1 to 2

---
+++ 0x02-i18n/2-app.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Get locale from request
+"""
+from flask import Flask, render_template, request
+from flask_babel import Babel
+
+
+class Config:
+    """Representation of Babel configuration
+    """
+    LANGUAGES = ['en', 'fr']
+    BABEL_DEFAULT_LOCALE = 'en'
+    BABEL_DEFAULT_TIMEZONE = 'UTC'
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.url_map.strict_slashes = False
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    """Get locale from request
+    """
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/')
+def say_hello():
+    """Route handler for 2-index.html
+    """
+    return render_template('2-index.html')
+
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port="3000")
